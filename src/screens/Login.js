723fad574eaf95879deaf613def7f14231caf9cf
Anwar Khan <anwarhasan002@gmail.com>
2026-04-10 14:47:22 +0530

    Reapply "Add BackHandler"
    This reverts commit 159658232e3254faa3b11c315c8ddc66d149a993.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -274,9 +274,6 @@ const Login = ({ navigation }) => {
       if (available) {
         const enrolled = await isEnrolled();
         setBiometricEnrolled(enrolled);
-
-        // Auto-prompt biometric login if already enrolled
-        // if (enrolled) handleBiometricLogin();
       }
     })();
   }, []);
